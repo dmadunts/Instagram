6070ed80b64e8c75510f5b2f63ea8838452e8388
David <madunts.d@gmail.com>
2018-12-11 11:13:11 +0500

    Init commit

---
+++ app/src/main/java/com/example/renai/instagram/HomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.renai.instagram
+
+import android.os.Bundle
+import android.util.Log
+
+class HomeActivity : BaseActivity(0) {
+    private val TAG = "HomeActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        Log.d(TAG, "onCreate")
+        setupBottomNavigation()
+    }
+}
