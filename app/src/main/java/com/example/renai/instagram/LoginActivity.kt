d396db1a7742b51cd08813db8c1869b18ba0cdc7
David <madunts.d@gmail.com>
2018-12-18 14:04:03 +0500

    Added scrolling when keyboard is open on LoginActivity. Added states and animation to Login button

--- app/src/main/java/com/example/renai/instagram/LoginActivity.kt
+++ app/src/main/java/com/example/renai/instagram/LoginActivity.kt
@@ -2,15 +2,50 @@ package com.example.renai.instagram
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.view.View
+import com.example.renai.instagram.R.style.login_button
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEvent
+import net.yslibrary.android.keyboardvisibilityevent.KeyboardVisibilityEventListener
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KeyboardVisibilityEventListener, TextWatcher {
+
+
     private val TAG = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         Log.d(TAG, "onCreate")
+        KeyboardVisibilityEvent.setEventListener(this, this)
+
+        login_btn.isEnabled = false
+        email_input.addTextChangedListener(this)
+        password_input.addTextChangedListener(this)
     }
+
+    override fun onVisibilityChanged(isKeyboardOpen: Boolean) {
+        if (isKeyboardOpen) {
+            scrollView.scrollTo(0, scrollView.bottom)
+            create_account_text.visibility = View.GONE
+        } else {
+            scrollView.scrollTo(0, scrollView.top)
+            create_account_text.visibility = View.VISIBLE
+        }
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        login_btn.isEnabled = email_input.text.toString().isNotEmpty() && password_input.text.toString().isNotEmpty()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
 }
