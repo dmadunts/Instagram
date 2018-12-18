de623aef1ceeb7cbe4ecdea524660d5616d64a04
David <madunts.d@gmail.com>
2018-12-19 00:59:45 +0500

    Started work on registration

--- app/src/main/java/com/example/renai/instagram/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/renai/instagram/activities/RegisterActivity.kt
@@ -1,10 +1,13 @@
 package com.example.renai.instagram.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.renai.instagram.R
-import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.fragment_register_email.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -19,11 +22,21 @@ class RegisterActivity : AppCompatActivity() {
 
 //1 - Email, next button
 class EmailFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register_email, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        next_btn.setOnClickListener{
+            val email = email_input.text.toString()
+
+        }
+
+    }
 }
 
+
+//2 - Full name, password, register button
 class NamePassFragment : Fragment() {
 
 }
-
-//2 - Full name, password, register button
