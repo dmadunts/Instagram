a6318456f9e833dda220bbd647b49ace4b4110d5
David <madunts.d@gmail.com>
2018-12-19 18:03:52 +0500

    Some changes in RegisterActivity (Not finished).

--- app/src/main/java/com/example/renai/instagram/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/renai/instagram/activities/RegisterActivity.kt
@@ -1,42 +1,125 @@
 package com.example.renai.instagram.activities
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.renai.instagram.R
+import com.example.renai.instagram.models.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_register_email.*
+import kotlinx.android.synthetic.main.fragment_register_namepass.*
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), EmailFragment.Listener, NamePassFragment.Listener {
+    private val TAG = "RegisterActivity"
+    private var mEmail: String? = null
+    private lateinit var mAuth:FirebaseAuth
+    private lateinit var mDatabase:DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance().reference
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction().add(R.id.frame_layout, EmailFragment()).commit()
         }
+    }
+
+    override fun onNext(email: String) {
+        mEmail = email
+        if (email.isNotEmpty()) {
+            supportFragmentManager.beginTransaction().replace(R.id.frame_layout, NamePassFragment())
+                .addToBackStack(null)
+                .commit()
+        } else {
+            showToast("Please enter E-mail")
+        }
+    }
+
+    override fun onRegister(fullName: String, password: String) {
+        if (fullName.isNotEmpty() && password.isNotEmpty()) {
+            val email = mEmail
+            if (email != null) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener{
+                        if (it.isSuccessful) {
+                            val username = mkUsername(fullname)
+                            val user = User(name = fullName, username = username, email = email)
+                            mDatabase.child("users").child(it.result.user.uid).setValue()
+                        }
+                    }
+            } else {
+                Log.e(TAG, "onRegister: e-mail is null")
+                showToast("Please enter e-mail")
+                supportFragmentManager.popBackStack()
+            }
+        } else {
+            showToast("Please enter e-mail and password")
+        }
+    }
+
+    private fun mkUsername(fullname: String): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
 
 //1 - Email, next button
 class EmailFragment : Fragment() {
+    private lateinit var mListener: Listener
+
+    interface Listener {
+        fun onNext(email: String)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register_email, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        next_btn.setOnClickListener{
+        next_btn.setOnClickListener {
             val email = email_input.text.toString()
-
+            mListener.onNext(email)
         }
+    }
 
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        mListener = context as Listener
     }
 }
 
 
 //2 - Full name, password, register button
 class NamePassFragment : Fragment() {
+    private lateinit var mListener: Listener
 
+    interface Listener {
+        fun onRegister(fullName: String, password: String)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_register_namepass, container, false)
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        mListener = context as Listener
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        register_btn.setOnClickListener {
+            val fullName = register_name_input.text.toString()
+            val password = register_password_input.text.toString()
+            mListener.onRegister(fullName, password)
+        }
+    }
 }
