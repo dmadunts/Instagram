d3ff4106678db379c0810edc956fa0c2fdbe5cb8
David <madunts.d@gmail.com>
2018-12-21 17:53:43 +0500

    Started working on Profile editing

--- app/src/main/java/com/example/renai/instagram/activities/EditProfileActivity.kt
+++ app/src/main/java/com/example/renai/instagram/activities/EditProfileActivity.kt
@@ -9,19 +9,18 @@ import android.widget.TextView
 import com.example.renai.instagram.R
 import com.example.renai.instagram.models.User
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_edit_profile.*
 
 class EditProfileActivity : AppCompatActivity() {
     private val TAG = "EditProfileActivity"
+    private lateinit var mUser: User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         Log.d(TAG, "onCreate")
 
+        save_image.setOnClickListener { updateProfile() }
         close_image.setOnClickListener { finish() }
 
         //Spinner
@@ -35,15 +34,42 @@ class EditProfileActivity : AppCompatActivity() {
                 val database = FirebaseDatabase.getInstance().reference
                 database.child("users").child(auth.currentUser!!.uid)
                     .addListenerForSingleValueEvent(ValueEventListenerAdapter {
-                        val user = it.getValue(User::class.java)
-                        name_input.setText(user!!.name, TextView.BufferType.EDITABLE)
-                        username_input.setText(user.username, TextView.BufferType.EDITABLE)
-                        bio_input.setText(user.bio, TextView.BufferType.EDITABLE)
-                        website_input.setText(user.website, TextView.BufferType.EDITABLE)
-                        phone_number_input.setText(user.phone.toString(), TextView.BufferType.EDITABLE)
-                        email_input.setText(user.email, TextView.BufferType.EDITABLE)
+                        mUser = it.getValue(User::class.java)!!
+                        name_input.setText(mUser.name, TextView.BufferType.EDITABLE)
+                        username_input.setText(mUser.username, TextView.BufferType.EDITABLE)
+                        bio_input.setText(mUser.bio, TextView.BufferType.EDITABLE)
+                        website_input.setText(mUser.website, TextView.BufferType.EDITABLE)
+                        phone_input.setText(mUser.phone.toString(), TextView.BufferType.EDITABLE)
+                        email_input.setText(mUser.email, TextView.BufferType.EDITABLE)
                     })
             }
     }
+
+    private fun updateProfile() {
+        //get user from inputs
+        //validate
+        val user = User(
+            name = name_input.text.toString(),
+            username = username_input.text.toString(),
+            bio = bio_input.text.toString(),
+            website = website_input.text.toString(),
+            phone = phone_input.text.toString().toLong(),
+            email = email_input.text.toString()
+        )
+        val error = validate(user)
+        if (error == null) {
+            //save user
+        } else {
+            showToast(error)
+        }
+    }
+
+    private fun validate(user: User): String? =
+        when {
+            user.name.isEmpty() -> "Please enter your Name"
+            user.username.isEmpty() -> "Please enter your Username"
+            user.email.isEmpty() -> "Please enter your E-mail"
+            else -> null
+        }
 }
 
