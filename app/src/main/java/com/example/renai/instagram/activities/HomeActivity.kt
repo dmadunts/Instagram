40180ba58704fe28d09debc3ebe5cd1776e76992
David <madunts.d@gmail.com>
2019-01-09 18:02:30 +0500

    Keep working on Feed

--- app/src/main/java/com/example/renai/instagram/activities/HomeActivity.kt
+++ app/src/main/java/com/example/renai/instagram/activities/HomeActivity.kt
@@ -1,12 +1,24 @@
 package com.example.renai.instagram.activities
 
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.renai.instagram.R
 import com.example.renai.instagram.utils.FirebaseHelper
+import com.example.renai.instagram.utils.GlideApp
 import com.example.renai.instagram.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.feed_item.view.*
 
 class HomeActivity : BaseActivity(0) {
     private val TAG = "HomeActivity"
@@ -30,7 +42,7 @@ class HomeActivity : BaseActivity(0) {
 
     override fun onStart() {
         super.onStart()
-        val currentUser  = mFirebase.auth.currentUser
+        val currentUser = mFirebase.auth.currentUser
         if (currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
@@ -38,8 +50,41 @@ class HomeActivity : BaseActivity(0) {
             mFirebase.database.child("feed-posts").child(currentUser.uid)
                 .addValueEventListener(ValueEventListenerAdapter {
                     val posts = it.children.map { it.getValue(FeedPost::class.java)!! }
-                    Log.d(TAG, "feedPosts: ${posts.first().timestampDate()}")
+                    feed_recycler.adapter = FeedAdapter(posts)
+                    feed_recycler.layoutManager = LinearLayoutManager(this)
                 })
         }
+    }
+}
+
+class FeedAdapter(private val posts: List<FeedPost>) : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.feed_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = posts.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = posts[position]
+        with(holder) {
+            view.user_photo_image.loadImage(post.photo)
+            view.username_text.text = post.username
+            view.post_image.loadImage(post.image)
+            if (post.likesCount == 0) {
+                view.likes_text.visibility = View.GONE
+            } else {
+                view.likes_text.visibility = View.VISIBLE
+                view.likes_text.text = "${post.likesCount} likes"
+            }
+            val usernameSpannable = SpannableString(post.username)
+            usernameSpannable.setSpan(StyleSpan(Typeface.BOLD), 0, usernameSpannable.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+    }
+
+    private fun ImageView.loadImage(image: String?) {
+        GlideApp.with(this).load(image).centerCrop().into(this)
     }
 }
